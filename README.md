5b1eb73c075870a69596a00c9f4d23f9c04b5c6e
AngryHickory <138169281+AngryHickory@users.noreply.github.com>
2025-05-08 15:37:14 -0700

    Set up js file to only run when DOM fully loaded
    added event listener so the DOM will fully load before the script runs,
    declared 16 rows and columns for the grid

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # etch
 Etch-a-Sketch style game
+
+
+Logic: 
+1. get a reference to grid container
+2. loop to create 16 rows
+   row = 0, if row is less than 16, add 1
---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function() {
+    let gridContainer = document.getElementById("gridContainer");
+    console.log(gridContainer);
+});
+
+const numRows = 16;
+const numCols = 16;
